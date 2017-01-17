6c37217e1dba5bddacf9fb0a3cea954a65092e61
yazan <yazan.bakour@ipricegroup.com>
2017-01-17 11:19:54 +0800

    reformt main js file

--- sources/main.js
+++ sources/main.js
@@ -1,12 +1,11 @@
-/**
- * Created by yazan on 1/12/17.
- */
+/** Created by yazan on 1/12/17. **/
+
 
 /** Blogging Competition **/
 
 //scroll to top button
-$( document ).ready(function() {
-    $('.go-top').click(function() {
+$(document).ready(function () {
+    $('.go-top').click(function () {
         $('body,html').animate({
             scrollTop: 0
         }, 'slow');
@@ -27,14 +26,8 @@ $( document ).ready(function() {
     });
 
     //expand accordion once button clicked to show more details
-    $(document).ready(function() {
-
-        $(document).ready(function(){
-            $('.clicked').click(function(){
-                $(this).next('.mr-details').slideToggle("fast");
-            });
-
-        });
+    $('.clicked').click(function () {
+        $(this).next('.mr-details').slideToggle("fast");
     });
 
     //tag navigation buttons with belong sections
@@ -44,7 +37,7 @@ $( document ).ready(function() {
     $(window).on('scroll', function () {
         var cur_pos = $(this).scrollTop();
 
-        $("#content > section").each(function() {
+        $("#content > section").each(function () {
             var top = $(this).offset().top - nav_height,
                 bottom = top + $(this).outerHeight();
 
@@ -54,25 +47,28 @@ $( document ).ready(function() {
 
                 $("#content > section").removeClass('active');
 
-                nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+                nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
             }
         });
     });
 
     //fixed sticky navigation menu once start scroll
 
-    var quart = heightScrollTop = 171; /* the browser height calculation 1800 in pixel start from the top */
-    var calc = heightScrollTop = 240; /* the browser height calculation 240 in pixel start from the top */
-    var half = heightScrollTop = 1800; /* the browser height calculation 1800 in pixel start from the top */
+    var quart = heightScrollTop = 171;
+    /* the browser height calculation 1800 in pixel start from the top */
+    var calc = heightScrollTop = 240;
+    /* the browser height calculation 240 in pixel start from the top */
+    var half = heightScrollTop = 1800;
+    /* the browser height calculation 1800 in pixel start from the top */
 
-    $(window).bind('scroll', function() {
+    $(window).bind('scroll', function () {
 
-        if($(window).scrollTop() > quart){
+        if ($(window).scrollTop() > quart) {
 
             $('.nav-menu, .go-top').addClass('fixed');
             $('#main-container > #header > .nav-menu > .wrapper > .go-top').addClass('show');
 
-        }else{
+        } else {
 
             $('.nav-menu, .go-top').removeClass('fixed');
             $('#main-container > #header > .nav-menu > .wrapper > .go-top').removeClass('show');
@@ -80,19 +76,19 @@ $( document ).ready(function() {
     });
 
     // fixed sticky form
-    $(window).bind('scroll', function() {
-        if ($(window).scrollTop() > calc){
+    $(window).bind('scroll', function () {
+        if ($(window).scrollTop() > calc) {
             $('.side-box').addClass('positioning');
 
-        }else{
+        } else {
             $('.side-box').removeClass('positioning');
         }
 
-        if ($(window).scrollTop() > half){
+        if ($(window).scrollTop() > half) {
             $('.side-box').addClass('static');
             $('.side-box').removeClass('positioning');
 
-        }else{
+        } else {
             $('.side-box').removeClass('static');
         }
 
@@ -102,57 +98,57 @@ $( document ).ready(function() {
             $('.side-box').addClass('relative');
         }
     });
-});
-
-/** Merchant Award **/
-
-//iframe parameter link
-var iframe = document.createElement('iframe');
-
-function getQueryString() {
-    var search = location.search ;
-    if (search !== "") {
-        return search.substr(1).split('&').reduce(function (acc, curr) {
-            var kv = curr.split('=');
-            acc[kv[0]] = kv[1];
-            return acc;
-        }, {});
-    } else {
-        return "";
-    }
-}
-
-var arr = getQueryString();
-
-if(arr !== ""){
-    // Show custom iframe with indivdual store ratings
-    $('.tc-search').css('display', 'none');
-    $('#review_form').prop("action", "https://trustedcompany.com/profile/" + arr.mid + "/review");
-    $('#m-logo').text("" + arr.mname);
-} else {
-    $(".stars").css('display','none');
-}
 
 //read more and read less button trigger
-$(document).ready(function() {
-    $('.read-more > a').click(function() {
+    $('.read-more > a').click(function () {
         var that = $(this);
-        $('.hidden, .cover').slideToggle(function(){
-            if($('.hidden').is(':visible')){
+        $('.hidden, .cover').slideToggle(function () {
+            if ($('.hidden').is(':visible')) {
                 that.find('p').text('Read Less')
-            }else {
+            } else {
                 that.find('p').text('Read More')
             }
         });
     });
+
+    /** Merchant Award **/
+
+//iframe parameter link
+    var iframe = document.createElement('iframe');
+
+    function getQueryString() {
+        var search = location.search;
+        if (search !== "") {
+            return search.substr(1).split('&').reduce(function (acc, curr) {
+                var kv = curr.split('=');
+                acc[kv[0]] = kv[1];
+                return acc;
+            }, {});
+        } else {
+            return "";
+        }
+    }
+
+    var arr = getQueryString();
+
+    if (arr !== "") {
+        // Show custom iframe with indivdual store ratings
+        $('.tc-search').css('display', 'none');
+        $('#review_form').prop("action", "https://trustedcompany.com/profile/" + arr.mid + "/review");
+        $('#m-logo').text("" + arr.mname);
+    } else {
+        $(".stars").css('display', 'none');
+    }
+
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//trustedcompany.com/statics/scripts/plugins.js?locale=en";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'tc-plugins'));
+
+
 });
 
-
-(function (d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) return;
-    js = d.createElement(s);
-    js.id = id;
-    js.src = "//trustedcompany.com/statics/scripts/plugins.js?locale=en";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'tc-plugins'));
